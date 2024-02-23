eebb94536e4474719423d54b7a97aa9475fa8d36
André de Lima Alves <141466018+devandrelima@users.noreply.github.com>
2024-02-23 13:17:22 -0300

    Update ContaTerminal.java

--- desafio/ContaTerminal.java
+++ desafio/ContaTerminal.java
@@ -1,13 +1,10 @@
 package java.desafio.view;
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
-
-	static Scanner ler = new Scanner(System.in);
-
+	
 	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
 		ContaBanco contaBanco = new ContaBanco();
 
 		System.out.print("Nome Cliente: ");
@@ -16,60 +13,34 @@ public class ContaTerminal {
 		System.out.print("Agência do Banco: ");
 		contaBanco.setAgencia(ler.nextLine());
 
-		contaBanco.setNumeroConta(preencherNumeroConta());
+		contaBanco.setNumeroConta((int)validarNumero(ler, "Número da Conta: ", "\nEntrada Inválida! Tente Novamente.\n\n"));
 
-		contaBanco.setSaldo(preencherSaldo());
+		contaBanco.setSaldo(validarNumero(ler, "Saldo: ", "\nSaldo Inválido! Tente Novamente.\n\n"));
 
 		System.out.print("\nOlá " + contaBanco.getNomeCliente() + ", obrigado por criar uma conta em nosso banco, \n"
 				+ "sua agência é " + contaBanco.getAgencia() + ", conta " + contaBanco.getNumeroConta()
 				+ "\ne seu saldo de R$" + contaBanco.getSaldo() + " reais já está disponível para saque.");
 	}
 	
-	//------------------------------------------------------------------------------------------
-	// Descrição_: Função que trata erros durante o preenchimento da entrada do número da conta.
-	// Entrada___: Sem parâmetos.
-	// Saída_____: Retorna um número inteiro.
-	//------------------------------------------------------------------------------------------
-	static int preencherNumeroConta() {
-		boolean sair;
-		int numeroConta = 0;
-
-		do {
-			sair = false;
-			System.out.print("Número da Conta: ");
-			try {
-				numeroConta = ler.nextInt();
-			} catch (InputMismatchException e) {
-				sair = true;
-				System.out.print("\nInválido! Tente Novamente com número inteiro!\n\n");
-				ler.next();
-			}
-		} while (sair);
-
-		return numeroConta;
-	}
-	
-	//----------------------------------------------------------------------------------------
-	// Descrição_:Função que trata erros durante o preenchimento da entrada do saldo da conta.
+	//------------------------------------------------------------------------------
+	// Descrição_:Função que trata erros durante o preenchimento de dados numéricos.
 	// Entrada___: Sem parâmetos.
 	// Saída_____: Retorna um número double.
-	//----------------------------------------------------------------------------------------
-	static double preencherSaldo() {
-		boolean sair;
-		double saldo = 0.0;
+	//------------------------------------------------------------------------------
+	static double validarNumero(Scanner ler, String rotulo, String mensagem) {
+		double numeroConta = 0;
 
-		do {
-			sair = false;
-			System.out.print("Saldo: ");
+		while(true) {
+			System.out.print(rotulo);
 			try {
-				saldo = ler.nextDouble();
-			} catch (InputMismatchException e) {
-				sair = true;
-				System.out.print("\nSaldo Inválido! Tente Novamente!\n\n");
+				numeroConta = ler.nextInt();
+				break;
+			} catch (Exception e) {
+				System.out.print(mensagem);
 				ler.next();
 			}
-		} while (sair);
+		}
 
-		return saldo;
+		return numeroConta;
 	}
 }
